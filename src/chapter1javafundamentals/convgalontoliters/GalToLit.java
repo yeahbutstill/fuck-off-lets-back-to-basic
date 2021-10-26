4d1b603c443ad607743f4b343fb8f8bf3f61e483
Dani Setiawan <mayadani28@gmail.com>
2021-10-26 19:20:28 +0700

    yeah finis chapter 1

---
+++ src/chapter1javafundamentals/convgalontoliters/GalToLit.java
@@ -0,0 +1,14 @@
+package chapter1javafundamentals.convgalontoliters;
+
+public class GalToLit {
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+
+    }
+}
