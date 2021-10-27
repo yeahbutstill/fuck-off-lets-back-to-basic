767f5884fbd092f67e7af55be284a6891160ece3
Dani Setiawan <mayadani28@gmail.com>
2021-10-27 08:17:14 +0700

    test chapter 1

---
+++ src/chapter1javafundamentals/moon/Moon.java
@@ -0,0 +1,13 @@
+package chapter1javafundamentals.moon;
+
+public class Moon {
+    public static void main(String[] args) {
+        double earthWeight; // weight on earth
+        double moonWeight; // weight on moon
+
+        earthWeight = 165;
+        moonWeight = earthWeight * 0.17;
+
+        System.out.println(earthWeight + " earth-pounds is equivalent to " + moonWeight + " moon-pounds.");
+    }
+}
