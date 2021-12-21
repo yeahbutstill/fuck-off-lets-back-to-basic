57db5c113e534d989069e8c2cc06b68db62897c5
Dani Setiawan <mayadani28@gmail.com>
2021-12-21 13:10:35 +0700

    chapter 5 two demension array

--- src/chapter5moredatatypesandoperators/arrayonedimension/trythissortinganarray/Bubble.java
+++ src/chapter5moredatatypesandoperators/arrayonedimension/trythissortinganarray/Bubble.java
@@ -17,25 +17,25 @@ public class Bubble {
 
         // display original array
         System.out.print("Original array is:");
-        for (int i=0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             System.out.print(" " + nums[i]);
         }
         System.out.println();
 
         // This is the Bubble sort.
-        for (a=1; a<size; a++)
-            for (b=size-1; b>=a; b--) {
-                if (nums[b-1]>nums[b]) { // if out of order
+        for (a = 1; a < size; a++)
+            for (b = size - 1; b >= a; b--) {
+                if (nums[b - 1] > nums[b]) { // if out of order
                     // exchange elements
-                    t = nums[b-1];
-                    nums[b-1] = nums[b];
+                    t = nums[b - 1];
+                    nums[b - 1] = nums[b];
                     nums[b] = t;
                 }
             }
 
         // display sorted array
         System.out.print("Sorted array is:");
-        for (int i=0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             System.out.print(" " + nums[i]);
         }
         System.out.println();
