c08b56b97a2c8d5df90aa51d74804d34f78d2ce4
Dani Setiawan <mayadani28@gmail.com>
2021-12-23 08:02:45 +0700

    chapter 5 fucking awesome yeah

---
+++ src/chapter5moredatatypesandoperators/arrayonedimension/NoZeroDiv.java
@@ -0,0 +1,15 @@
+package chapter5moredatatypesandoperators.arrayonedimension;
+
+// Prevent a division by zero using the ?
+public class NoZeroDiv {
+    public static void main(String[] args) {
+        int result;
+
+        for(int i = -5; i < 6; i++) {
+            result = i != 0 ? 100 / i : 0; // this prevents a divide-by-zero.
+            if (i != 0) {
+                System.out.println("100 / " + i + " is " + result);
+            }
+        }
+    }
+}
