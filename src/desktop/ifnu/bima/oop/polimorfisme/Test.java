f458a9530af6c83a319a9861352d7dbcc9e50ec4
yeahbutstill <mayadani28@gmail.com>
2022-02-04 14:03:26 +0700

    polimorfisme

---
+++ src/desktop/ifnu/bima/oop/polimorfisme/Test.java
@@ -0,0 +1,30 @@
+package desktop.ifnu.bima.oop.polimorfisme;
+
+import desktop.ifnu.bima.oop.inheritance.Customer;
+import desktop.ifnu.bima.oop.inheritance.MemberCustomer;
+
+public class Test {
+  private static final String ACTION_1 = "tipenya Object";
+
+  public static void main(String[] args) {
+    Object o = new MemberCustomer();
+    Customer c = new MemberCustomer();
+    MemberCustomer mc = new MemberCustomer();
+    Object object = mc;
+    Customer cust = mc;
+
+    if (o instanceof Object) {
+      System.out.println(ACTION_1);
+    } else if (c instanceof Customer) {
+      System.out.println(ACTION_1);
+    } else if (mc instanceof Customer) {
+      System.out.println(ACTION_1);
+    } else if (object instanceof MemberCustomer) {
+      System.out.println(ACTION_1);
+    } else if (cust instanceof Object) {
+      System.out.println(ACTION_1);
+    } else {
+      System.out.println("bukan object yang sama");
+    }
+  }
+}
