d503aba21149131a8b1eeb678a522d38e7e8a51a
yeahbutstill <mayadani28@gmail.com>
2022-02-04 08:56:25 +0700

    throw exception test

--- src/desktop/ifnu/bima/throwsandthrow/ThrowsExceptionTest1.java
+++ src/desktop/ifnu/bima/throwsandthrow/ThrowsExceptionTest1.java
@@ -1,23 +1,29 @@
 package desktop.ifnu.bima.throwsandthrow;
 
+/**
+ * di dalam method methodTidakBertanggungJawab, exception tidak dihandle tetapi dilempar ke metod
+ * pemanggilnya dengan menggunakan sintaks throws. Sedangkan method main yang memanggil method
+ * methodTidakBertanggungJawab, akhirnya menghandle exception sehingga eksekusi aplikasi tidak
+ * langsung keluar dan dilanjutkan dengan mecetak string di bawahnya baru kemudian keluar.
+ */
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ThrowsExceptionTest1 {
-    public static void main(String[] args) {
-        try {
-            methodTidakBertanggungJawab();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("kode di dalam method main");
+  public static void main(String[] args) {
+    try {
+      methodTidakBertanggungJawab();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    System.out.println("kode di dalam method main");
+  }
 
-    public static void methodTidakBertanggungJawab() throws FileNotFoundException, IOException {
-        FileInputStream fileInputStream = new FileInputStream("buka-file.txt");
-        System.out.println("kode setelah buka file");
-    }
+  public static void methodTidakBertanggungJawab() throws FileNotFoundException, IOException {
+    FileInputStream fileInputStream = new FileInputStream("buka-file.txt");
+    System.out.println("kode setelah buka file");
+  }
 }
