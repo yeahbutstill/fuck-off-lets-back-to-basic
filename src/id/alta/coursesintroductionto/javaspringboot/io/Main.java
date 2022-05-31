bf6a34125ef9b807ff5cdd05d11c1c35f044e6ac
yeahbutstill <mayadani28@gmail.com>
2022-05-31 07:28:38 +0700

    input output

---
+++ src/id/alta/coursesintroductionto/javaspringboot/io/Main.java
@@ -0,0 +1,22 @@
+package id.alta.coursesintroductionto.javaspringboot.io;
+
+import java.util.Scanner;
+
+// output dan input
+public class Main {
+    public static void main(String[] args) {
+        Double number = -10.6;
+
+        System.out.println(5);
+        System.out.println(number);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number1 = input.nextInt();
+        System.out.println("You entered " + number1);
+
+        // closing the scanner object
+        input.close();
+
+    }
+}
