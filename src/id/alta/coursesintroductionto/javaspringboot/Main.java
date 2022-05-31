ac2839807d793ec60a8b6be5a6cdc32c86c7a506
yeahbutstill <mayadani28@gmail.com>
2022-05-31 07:11:58 +0700

    update

---
+++ src/id/alta/coursesintroductionto/javaspringboot/Main.java
@@ -0,0 +1,7 @@
+package id.alta.coursesintroductionto.javaspringboot;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/id/alta/coursesintroductionto/javaspringboot/conversitipedata/Main.java
@@ -0,0 +1,10 @@
+package id.alta.coursesintroductionto.javaspringboot.conversitipedata;
+
+public class Main {
+    public static void main(String[] args) {
+        // conversi tipe data
+        int umur = 10;
+        long umurPanjang = umur;
+        byte umurPendek = (byte) umur;
+    }
+}
