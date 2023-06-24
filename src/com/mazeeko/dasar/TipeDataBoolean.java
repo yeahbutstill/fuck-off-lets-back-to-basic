617988800bcb6a6e5d353620a56b2fd96ad34a10
yeahbutstill <mayadani28@gmail.com>
2023-06-24 12:31:51 +0700

    back to back

---
+++ src/com/mazeeko/dasar/TipeDataBoolean.java
@@ -0,0 +1,7 @@
+package com.mazeeko.dasar;
+public class TipeDataBoolean {
+    public static void main(String[] args){
+        boolean benar = true;
+        boolean salah = false;
+    }
+}
---
+++ src/com/mazeeko/dasar/TipeDataString.java
@@ -0,0 +1,25 @@
+package com.mazeeko.dasar;
+public class TipeDataString {
+    public static void main(String[] args){
+        String a = "A";
+        String b = "B";
+        String c = "C";
+        String d = "D";
+        String e = "E";
+        String f = "F";
+        String g = "G";
+        String h = "H";
+        String i = "I";
+        String j = "J";
+        String k = "K";
+        String l = "L";
+        String m = "M";
+        String n = "N";
+        String o = "O";
+        String p = "P";
+        String q = "Q";
+        String r = "R";
+        String s = "S";
+        String t = "T";
+    }
+}
