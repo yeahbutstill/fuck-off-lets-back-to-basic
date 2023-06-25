280f2e238bc755078424ca60a8223aa0489f0248
yeahbutstill <mayadani28@gmail.com>
2023-06-26 04:55:42 +0700

    fun

---
+++ src/com/mazeeko/dasar/OperasiBoolean.java
@@ -0,0 +1,22 @@
+package com.mazeeko.dasar;
+public class OperasiBoolean {
+    public static void main(String[] args){
+        Integer absen = 70;
+        Integer nilaiAkhir = 80;
+
+        boolean lulusAbsen = absen >= 75;
+        boolean lulusNilaiAkhir = nilaiAkhir >= 75;
+
+        // kalau && nilai kiri dan kanan harus true jika ingin hasilnya true
+        boolean lulus = lulusAbsen && lulusNilaiAkhir;
+        System.out.println(lulus); // karena absen kurang dari 75
+
+        // ini true karena || yang penting naila kiri dan kanan ada yang hasilnya true
+        boolean remedial = lulusAbsen || lulusNilaiAkhir;
+        System.out.println(remedial);
+
+        boolean dapetIzasah = false;
+        System.out.println(!dapetIzasah);
+
+    }
+}
---
+++ src/com/mazeeko/dasar/OperasiPerbandingan.java
@@ -0,0 +1,14 @@
+package com.mazeeko.dasar;
+public class OperasiPerbandingan {
+    public static void main(String[] args){
+        Integer a = 100;
+        Integer b = 100;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
