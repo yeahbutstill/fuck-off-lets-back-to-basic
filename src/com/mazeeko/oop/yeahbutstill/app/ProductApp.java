7dc039ca9f4e35b135b9e2c9d356d92401536abb
yeahbutstill <mayadani28@gmail.com>
2023-07-06 08:34:47 +0700

    fun

--- src/com/mazeeko/oop/yeahbutstill/app/ProductApp.java
+++ src/com/mazeeko/oop/yeahbutstill/app/ProductApp.java
@@ -9,6 +9,10 @@ public class ProductApp {
     product2.setName("Mac Book Pro");
     product2.setPrice(15_000_000);
 
+    String firstName = product1.getName();
+    String secondName = product2.getName();
+    String thirdName = "Mac Book Pro";
+
     System.out.println(product1);
     System.out.println(product2);
 
@@ -16,5 +20,11 @@ public class ProductApp {
     System.out.println(product1 == product2);
     // ini bandingkan isi 2 object
     System.out.println(product1.equals(product2));
+
+    // ini kenapa true, karena java akan optimasi memory, jadi kalau kita bikin string isi stringnya
+    // itu sama
+    // maka dia tidak akan membuat ulang data string di memory, makan dia akan merefer ke data
+    // object yang sudah ada
+    System.out.println(secondName == thirdName);
   }
 }
