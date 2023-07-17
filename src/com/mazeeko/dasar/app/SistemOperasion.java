dcacfa60a6cab09d3cf2b6e47fc74270389a753c
yeahbutstill <mayadani28@gmail.com>
2023-07-17 08:19:17 +0700

    fun

---
+++ src/com/mazeeko/dasar/app/SistemOperasion.java
@@ -0,0 +1,12 @@
+package com.mazeeko.dasar.app;
+
+public class SistemOperasion {
+    private String name;
+    private Integer age;
+
+    public static void main(String[] args) {
+        var gnome = new SistemOperasion();
+        gnome.name = "Prof Dani";
+        gnome.age = 1321;
+    }
+}
