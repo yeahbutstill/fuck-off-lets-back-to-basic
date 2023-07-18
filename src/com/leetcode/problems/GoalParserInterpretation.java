44f2acaf6eb4f97288e118d58e0e4579debd1f10
yeahbutstill <mayadani28@gmail.com>
2023-07-18 13:46:23 +0700

    fun

---
+++ src/com/leetcode/problems/GoalParserInterpretation.java
@@ -0,0 +1,23 @@
+package com.leetcode.problems;
+
+public class GoalParserInterpretation {
+    public static void main(String[] args) {
+        String command = "G()(al)";
+        Solution1678 solution1678 = new Solution1678();
+        System.out.println(solution1678.interpret(command));
+    }
+}
+
+class Solution1678 {
+    public String interpret(String command) {
+        StringBuilder S = new StringBuilder(command.length());
+        for(int i = 0 ; i < command.length() ; i++){
+            if(command.charAt(i)=='G') S.append('G');
+            if(command.charAt(i)=='('){
+                if(command.charAt(i+1)==')') {S.append('o'); i++;}
+                else{S.append("al"); i = i + 3;}
+            }
+        }
+        return S.toString();
+    }
+}
--- src/com/leetcode/problems/NumJewelsInStones.java
+++ src/com/leetcode/problems/NumJewelsInStones.java
@@ -10,28 +10,7 @@ public class NumJewelsInStones {
 }
 
 class Solution771 {
-    public int numJewelsInStones(String jewels, String stones) {
-        int res = 0;
-        int[] arr = new int[26];
-        int[] arr2 = new int[26];
-        for(int i = 0; i < stones.length(); i++) {
-            char c = stones.charAt(i);
-            if (Character.isLowerCase(c)){
-                arr[c - 'a']++;
-            } else {
-                arr2[c - 'A']++;
-            }
-        }
-
-        for(int i = 0; i < jewels.length(); i++) {
-            char c = jewels.charAt(i);
-            if (Character.isLowerCase(c)){
-                if (arr[c - 'a'] != 0 ) res += arr[c - 'a'];
-            } else {
-                if (arr2[c - 'A'] != 0 ) res += arr2[c - 'A'];
-            }
-        }
-
-        return res;
+    public int numJewelsInStones(String J, String S) {
+        return S.replaceAll("[^" + J + "]", "").length();
     }
 }
