f626777113f8f1ccf61ee551c1cde9c46bff2863
yeahbutstill <mayadani28@gmail.com>
2023-07-18 13:53:18 +0700

    fun

---
+++ src/com/leetcode/problems/MostWordsFound.java
@@ -0,0 +1,22 @@
+package com.leetcode.problems;
+
+public class MostWordsFound {
+    public static void main(String[] args) {
+        String[] sentences = new String[]{"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+        Solution2114 solution2114 = new Solution2114();
+        System.out.println(solution2114.mostWordsFound(sentences));
+    }
+}
+
+class Solution2114 {
+    public int mostWordsFound(String[] sentences) {
+        int maxLen = 0;
+
+        for (String currSent : sentences) {
+            int currLen = currSent.split(" ").length;
+            if (maxLen < currLen)
+                maxLen = currLen;
+        }
+        return maxLen;
+    }
+}
