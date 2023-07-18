13545a85682a396d4bb2461e63086348833d9a50
yeahbutstill <mayadani28@gmail.com>
2023-07-18 12:57:14 +0700

    fun

---
+++ src/com/leetcode/problems/DefanginganIPAddress.java
@@ -0,0 +1,15 @@
+package com.leetcode.problems;
+
+public class DefanginganIPAddress {
+    public static void main(String[] args) {
+        String ip = "1.1.1.1";
+        Solution1108 solution = new Solution1108();
+        System.out.println(solution.defangIPaddr(ip));
+    }
+}
+
+class Solution1108 {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
