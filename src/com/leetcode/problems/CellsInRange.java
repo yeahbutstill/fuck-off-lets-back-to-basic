34ba4f7b99348963c1f57f9569fee5e91dcc8ec1
yeahbutstill <mayadani28@gmail.com>
2023-07-18 17:03:50 +0700

    fun

---
+++ src/com/leetcode/problems/CellsInRange.java
@@ -0,0 +1,26 @@
+package com.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CellsInRange {
+    public static void main(String[] args) {
+        String s = "K1:L2";
+        Solution2194 solution2194 = new Solution2194();
+        System.out.println(solution2194.cellsInRange(s));
+    }
+}
+
+class Solution2194 {
+    public List<String> cellsInRange(String s) {
+        char c1 = s.charAt(0), c2 = s.charAt(3);
+        char r1 = s.charAt(1), r2 = s.charAt(4);
+        List<String> cells = new ArrayList<>();
+        for (char c = c1; c <= c2; ++c) {
+            for (char r = r1; r <= r2; ++r) {
+                cells.add("" + c + r);
+            }
+        }
+        return cells;
+    }
+}
