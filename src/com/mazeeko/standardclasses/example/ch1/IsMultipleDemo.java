f0809fc07475aac8d2f83f833047998a5ddfbf52
yeahbutstill <mayadani28@gmail.com>
2023-07-19 20:15:46 +0700

    fun

---
+++ src/com/mazeeko/standardclasses/example/ch1/IsMultipleDemo.java
@@ -0,0 +1,22 @@
+package com.mazeeko.standardclasses.example.ch1;
+
+import java.util.Scanner;
+
+/**
+ * R-1.3 Tulis metode pendek Java, isMultiple, yang membutuhkan dua nilai panjang, n dan m, dan mengembalikan true jika dan hanya jika n
+ * adalah kelipatan dari m, yaitu n = mi untuk bilangan bulat i.
+ */
+public class IsMultipleDemo {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("Please input two numbers for n and m: ");
+        long n = stdIn.nextLong();
+        long m = stdIn.nextLong();
+        System.out.println(isMultiple(n, m));
+    }
+
+    private static boolean isMultiple(long n, long m) {
+        if (m > n) return isMultiple(m, n);
+        return (n & m) == 0;
+    }
+}
