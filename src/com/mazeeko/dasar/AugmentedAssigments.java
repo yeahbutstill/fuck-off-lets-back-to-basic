a101942739effff2b8fdee710aa63487bdaa048a
yeahbutstill <mayadani28@gmail.com>
2023-07-20 13:40:34 +0700

    fun

---
+++ src/com/mazeeko/dasar/AugmentedAssigments.java
@@ -0,0 +1,12 @@
+package com.mazeeko.dasar;
+
+public class AugmentedAssigments {
+    public static void main(String[] args) {
+        Integer a = 10;
+        Integer b = 20;
+        a += 10; // a = a+10
+        b -= 10; // a = a-10
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
