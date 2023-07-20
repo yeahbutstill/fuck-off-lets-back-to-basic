9f79f7993529dad8997f97a4557e93333dd5f283
yeahbutstill <mayadani28@gmail.com>
2023-07-20 13:49:02 +0700

    fun

---
+++ src/com/mazeeko/dasar/UnaryOperator.java
@@ -0,0 +1,19 @@
+package com.mazeeko.dasar;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int d = +100; // positive
+        int e = -10; // negative
+
+        d++; // d = d+1
+        System.out.println(d);
+
+        d--; // d = d-1
+        System.out.println(d);
+
+        ++d;
+        System.out.println(d);
+
+        System.out.println(!true); // boolean kebalikan
+    }
+}
