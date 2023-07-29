e2f5db0d72a9a34932d6d8025d22d23932a3ce7c
yeahbutstill <mayadani28@gmail.com>
2023-07-29 19:02:21 +0700

    fun

--- src/com/mazeeko/dasar/HelloDate.java
+++ src/com/mazeeko/dasar/HelloDate.java
@@ -1,4 +1,10 @@
 package com.mazeeko.dasar;
 
+import java.util.Date;
+
 public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
 }
