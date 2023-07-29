ca3b7cde671b0fc02c57f63241994deef3659797
yeahbutstill <mayadani28@gmail.com>
2023-07-29 11:40:08 +0700

    fun

--- src/com/mazeeko/oop/ShapeOne.java
+++ src/com/mazeeko/oop/ShapeOne.java
@@ -1,7 +1,11 @@
 package com.mazeeko.oop;
 
+import java.util.ArrayList;
+
 public class ShapeOne {
 
+    // reference
+    private String s = "asdf";
     private String color;
 
     public ShapeOne() {}
@@ -9,11 +13,13 @@ public class ShapeOne {
     public void doSomething(ShapeOne shapeOne) {
         shapeOne.erase();
         shapeOne.draw();
-        
+
         Circle circle = new Circle();
         Triangle triangle = new Triangle();
         doSomething(circle);
         doSomething(triangle);
+
+        ArrayList<ShapeOne> shapeOnes = new ArrayList<>();
     }
 
     public static void draw() {
@@ -34,5 +40,22 @@ public class ShapeOne {
 
     public void setColor(String color) {
         this.color = color;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // kata kunci new mengatakan "jadikan saya yang baru dari objek ini"
+        String s = new String();
+
+        char c = 'x';
+        Character ch = c;
+
+        Character ch1 = 'z';
+        char c1 = ch1;
+
+        System.out.println(ch);
+        System.out.println(ch1);
+        System.out.println(c1);
     }
 }
