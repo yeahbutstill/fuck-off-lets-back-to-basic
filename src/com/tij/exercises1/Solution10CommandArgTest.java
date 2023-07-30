db83134ce12989f1cb2680dd8d6e28148005b838
yeahbutstill <mayadani28@gmail.com>
2023-07-30 15:23:33 +0700

    fun

---
+++ src/com/tij/exercises1/Solution10CommandArgTest.java
@@ -0,0 +1,9 @@
+package com.tij.exercises1;
+
+public class Solution10CommandArgTest {
+    public static void main(String[] args) {
+        System.out.println("args[0] = " + args[0]);
+        System.out.println("args[1] = " + args[1]);
+        System.out.println("args[2] = " + args[2]);
+    }
+}
---
+++ src/com/tij/exercises1/Solution8OneStaticTest.java
@@ -0,0 +1,37 @@
+package com.tij.exercises1;
+
+public class Solution8OneStaticTest {
+    public static void main(String[] args) {
+        System.out.println("StaticTest.i= " + StaticTest.i);
+        StaticTest st1 = new StaticTest();
+        StaticTest st2 = new StaticTest();
+        System.out.println("st1.i= " + st1.i);
+        System.out.println("st2.i= " + st2.i);
+
+        Incrementable.increment();
+        System.out.println("After Incrementable.increment() called: ");
+        System.out.println("st1.i = " + st1.i);
+        System.out.println("st2.i = " + st2.i);
+
+        Incrementable.increment();
+        System.out.println("After Incrementable.increment called: ");
+        System.out.println("st1.i = " + st1.i);
+        System.out.println("st2.i = " + st2.i);
+        st1.i = 3;
+        System.out.println("After st1.i = 3, ");
+        System.out.println("st1.i = " + st1.i);
+        System.out.println("st2.i = " + st2.i);
+        System.out.println("Create another StaticTest, st3.");
+
+        StaticTest st3 = new StaticTest();
+        System.out.println("st3.i = " + st3.i);
+    }
+}
+
+class StaticTest {
+    static int i = 47;
+}
+
+class Incrementable {
+    static void increment() { StaticTest.i++; }
+}
