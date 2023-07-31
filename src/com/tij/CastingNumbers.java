ed11dc839978f6e0b863ea1ebe9763639bbe7c11
yeahbutstill <mayadani28@gmail.com>
2023-07-31 17:00:28 +0700

    fun

---
+++ src/com/tij/CastingNumbers.java
@@ -0,0 +1,14 @@
+package com.tij;
+
+public class CastingNumbers {
+    public static void main(String[] args) {
+        double above = 0.7;
+        double below = 0.4;
+        float fAbove = 0.7f;
+        float fBelow = 0.4f;
+        System.out.println("(int) above: " + (int)above);
+        System.out.println("(int) below: " + (int)below);
+        System.out.println("(int) fAbove: " + (int)fAbove);
+        System.out.println("(int) fBelow: " + (int)fBelow);
+    }
+}
---
+++ src/com/tij/StringOperators.java
@@ -0,0 +1,17 @@
+package com.tij;
+
+public class StringOperators {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 1;
+        int z = 2;
+        String s = "x, y, z ";
+
+        System.out.println(s + x + y + z);
+        System.out.println(x + " " + s); // Converts x to a String
+
+        s += "(summed) = "; // Concatenation operator
+        System.out.println(s + (x + y + z));
+        System.out.println("" + x); // Shorthand for Integer.toString()
+    }
+}
