36cca7068f77fd557c4e894b32bb8b1b0a3cc325
yeahbutstill <mayadani28@gmail.com>
2023-07-31 10:40:43 +0700

    fun

---
+++ src/com/tij/AutoInc.java
@@ -0,0 +1,12 @@
+package com.tij;
+
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i : " + i);
+        System.out.println("++i : " + ++i); // Pre-increment
+        System.out.println("preincrement i : " + i);
+        System.out.println("i++ : " + i++); // Post-increment
+        System.out.println("postincrement i : " + i);
+    }
+}
