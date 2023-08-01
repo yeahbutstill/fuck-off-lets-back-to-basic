198db6e24128b4858a4f5b9ac08f017b2c0373e1
yeahbutstill <mayadani28@gmail.com>
2023-08-01 07:36:00 +0700

    fun

---
+++ src/com/tij/control/WhileTest.java
@@ -0,0 +1,14 @@
+package com.tij.control;
+
+public class WhileTest {
+    public static void main(String[] args) {
+        while (condition()) System.out.println("Inside 'while'");
+        System.out.println("Exited 'while'");
+    }
+
+    static boolean condition() {
+        boolean result = Math.random() < 0.99;
+        System.out.println(result + ". ");
+        return result;
+    }
+}
