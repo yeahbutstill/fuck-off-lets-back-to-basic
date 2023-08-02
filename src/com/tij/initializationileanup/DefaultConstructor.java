f002dcf6509f79a6f7657b7696e1ff0e8f2450e4
yeahbutstill <mayadani28@gmail.com>
2023-08-03 00:00:48 +0700

    fun

---
+++ src/com/tij/initializationileanup/DefaultConstructor.java
@@ -0,0 +1,9 @@
+package com.tij.initializationileanup;
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Bird b = new Bird(); //Default
+    }
+}
+
+class Bird{}
