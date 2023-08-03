a1ba6d5914e241bb3ecb9e62c04b1a9be94d1688
yeahbutstill <mayadani28@gmail.com>
2023-08-03 22:26:31 +0700

    fun

---
+++ src/com/tij/initializationileanup/ArrayClassObj.java
@@ -0,0 +1,17 @@
+package com.tij.initializationileanup;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayClassObj {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        Integer[] a = new Integer[rand.nextInt(20)];
+        System.out.println("length of a = " + a.length);
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rand.nextInt(500); // Autoboxing
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
