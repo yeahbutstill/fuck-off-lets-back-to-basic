3eedc064f353fae70c62fd0a0a43e939131e26e9
yeahbutstill <mayadani28@gmail.com>
2023-08-04 15:04:01 +0700

    fun

---
+++ src/com/tij/exercieses4/initializationcleanup/DefaultConstructorTest2.java
@@ -0,0 +1,21 @@
+package com.tij.exercieses4.initializationcleanup;
+
+// initialization/DefaultConstructorTest2.java
+// TIJ4 Chapter Initialization, Exercise 4, page 167
+/* Add an overloaded constructor to the previous exercise that takes a String argument and prints it along with your message.*/
+public class DefaultConstructorTest2 {
+    public static void main(String[] args) {
+        Kabayo2 k = new Kabayo2();
+        Kabayo2 k2 = new Kabayo2("tumutugtog ng gitars");
+    }
+}
+
+class Kabayo2 {
+    Kabayo2() {
+        System.out.println("isa lang kabayo");
+    }
+
+    Kabayo2(String s) {
+        System.out.println(s);
+    }
+}
