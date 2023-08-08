5a618b223279da13eb441dc1b1df2e0f3dab6cd4
yeahbutstill <mayadani28@gmail.com>
2023-08-08 14:41:17 +0700

    fun :alien:

--- src/com/mazeeko/dasar/Variable.java
+++ src/com/mazeeko/dasar/Variable.java
@@ -1,9 +1,9 @@
 package com.mazeeko.dasar;
 public class Variable {
     public static void main(String[] args) {
-        // ini engga bisa, wajib harus ada valuenya di variabel tersebut jika menggunakan var.
+        // ini engga bisa, wajib inisialisasi atau harus ada valuenya di variabel tersebut jika menggunakan var.
         // karena kalau tidak ada valuenya, maka akan error
-        // var name; // error
+        //! var name; // error
         var age = 30;
         var address = "Jakarta";
 
