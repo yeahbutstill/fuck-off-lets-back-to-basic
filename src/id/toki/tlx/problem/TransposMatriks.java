f26d53a95c2d4df3f9ec1511022da047363467b4
yeahbutstill <mayadani28@gmail.com>
2023-08-13 22:38:49 +0700

    fun :alien:

---
+++ src/id/toki/tlx/problem/TransposMatriks.java
@@ -0,0 +1,22 @@
+package id.toki.tlx.problem;
+
+import java.util.Scanner;
+
+public class TransposMatriks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        int g = sc.nextInt();
+        int h = sc.nextInt();
+        int i = sc.nextInt();
+
+        System.out.println(a + " " + d + " " + g);
+        System.out.println(b + " " + e + " " + h);
+        System.out.println(c + " " + f + " " + i);
+    }
+}
