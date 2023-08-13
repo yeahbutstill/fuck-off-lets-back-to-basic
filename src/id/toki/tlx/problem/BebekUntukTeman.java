93aa251fdd6e300fb04c5bb5bad2da6984ef9ef6
yeahbutstill <mayadani28@gmail.com>
2023-08-13 16:27:58 +0700

    fun :alien:

---
+++ src/id/toki/tlx/problem/BebekUntukTeman.java
@@ -0,0 +1,14 @@
+package id.toki.tlx.problem;
+
+import java.util.Scanner;
+
+public class BebekUntukTeman {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x,y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("masing-masing " + x/y);
+        System.out.println("bersisa " + x%y);
+    }
+}
---
+++ src/id/toki/tlx/problem/BurungBeo.java
@@ -0,0 +1,12 @@
+package id.toki.tlx.problem;
+
+import java.util.Scanner;
+
+public class BurungBeo {
+    public static void main(String[] args) {
+        String s = " ";
+        Scanner sc = new Scanner(System.in);
+        s = sc.nextLine();
+        System.out.println(s);
+    }
+}
