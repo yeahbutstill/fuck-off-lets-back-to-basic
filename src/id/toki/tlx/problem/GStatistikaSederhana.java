46694b68824bc722abffc2373e8e8f8e8a4691a1
yeahbutstill <mayadani28@gmail.com>
2023-08-17 13:03:27 +0700

    fun :alien:

--- src/id/toki/tlx/problem/GStatistikaSederhana.java
+++ src/id/toki/tlx/problem/GStatistikaSederhana.java
@@ -8,11 +8,13 @@ public class GStatistikaSederhana {
         int input = sc.nextInt();
         int max = Integer.MAX_VALUE;
         int min = Integer.MIN_VALUE;
+
         for (int i = 0; i < input; i++) {;
             int num = sc.nextInt();
             if (num < max) max = num;
             if (num > min) min = num;
         }
+
         System.out.println(min + " " + max);
     }
 }
