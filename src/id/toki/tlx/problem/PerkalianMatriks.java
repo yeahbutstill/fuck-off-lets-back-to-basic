e3ab50b689c7cf70da1112544b99e95a15c772ca
yeahbutstill <mayadani28@gmail.com>
2023-08-17 16:59:57 +0700

    fun :alien:

---
+++ src/id/toki/tlx/problem/PerkalianMatriks.java
@@ -0,0 +1,42 @@
+package id.toki.tlx.problem;
+
+import java.util.Scanner;
+
+public class PerkalianMatriks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int p = sc.nextInt();
+
+        int[][] array1 = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                array1[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] array2 = new int[m][p];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < p; j++) {
+                array2[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] ans = new int[n][p];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < p; j++) {
+                for (int k = 0; k < m; k++) {
+                    ans[i][j] += array1[i][k] * array2[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < p; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
