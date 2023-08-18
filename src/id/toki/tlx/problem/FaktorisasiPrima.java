2597733ba8489a171c2ddf14ac925f82fcc66a0e
yeahbutstill <mayadani28@gmail.com>
2023-08-18 07:24:02 +0700

    fun :alien:

---
+++ src/id/toki/tlx/problem/FaktorisasiPrima.java
@@ -0,0 +1,25 @@
+package id.toki.tlx.problem;
+
+import java.util.Scanner;
+
+public class FaktorisasiPrima {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 2; i <= n; i++) {
+            int power = 0;
+            while (n % i == 0) {
+                n/=i;
+                power++;
+            }
+
+            if (power > 0) {
+                System.out.print(i);
+                if (power > 1) System.out.print("^" + power);
+                if (n > 1) {
+                    System.out.print(" x ");
+                }
+            }
+        }
+    }
+}
