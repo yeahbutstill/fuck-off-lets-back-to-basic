79ffd16eab7de8ac65d8ac3d77b0c3a81d7666f2
yeahbutstill <mayadani28@gmail.com>
2023-09-01 22:12:57 +0700

    perulangan

---
+++ src/id/toki/tlx/algo/PerulanganForSatu.java
@@ -0,0 +1,11 @@
+package id.toki.tlx.algo;
+
+public class PerulanganForSatu {
+    public static void main(String[] args) {
+        System.out.println("Perulangan Menggunakan For");
+        System.out.println("==============================");
+        for (int x = 5; x <= 25; x+=5) {
+            System.out.println("Angka Perulangan: " + x);
+        }
+    }
+}
---
+++ src/id/toki/tlx/algo/PerulanganWhileSatu.java
@@ -0,0 +1,13 @@
+package id.toki.tlx.algo;
+
+public class PerulanganWhileSatu {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Perulangan Menggunakan While");
+        System.out.println("=============================");
+        while (i <= 10) {
+            System.out.println("Angka Perulangan : " + i);
+            i++;
+        }
+    }
+}
