3ab85da1508c81531e74eb823ecbacbd25bbe499
yeahbutstill <mayadani28@gmail.com>
2023-09-03 13:54:30 +0700

    prosedure

---
+++ src/id/toki/tlx/algo/DekAksesArraySatuDimensi.java
@@ -0,0 +1,27 @@
+package id.toki.tlx.algo;
+
+public class DekAksesArraySatuDimensi {
+    public static void main(String[] args) {
+        /**
+         * Deklarasi
+         * berat_badan : array [1..30] of real
+         */
+        double[] beratBadan = new double[30];
+
+        /**
+         * Akses
+         * 1. Mengakses nilai
+         * array dengan indeks
+         * tertentu
+         */
+        double berat1 = beratBadan[0];
+
+        /**
+         * Mengisi nilai array
+         * dengan indeks
+         * tertentu
+         */
+        beratBadan[1] = 67;
+        berat1 = 10;
+    }
+}
---
+++ src/id/toki/tlx/algo/Sapa.java
@@ -0,0 +1,11 @@
+package id.toki.tlx.algo;
+
+public class Sapa {
+    public static void main(String[] args) {
+        sapaSaya();
+    }
+
+    private static void sapaSaya() {
+        System.out.println("Selamat di Jurusan Teknik Komputer");
+    }
+}
