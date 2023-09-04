3e8a14d20f7f8155d9f813b54f55c664a0e1fa83
yeahbutstill <mayadani28@gmail.com>
2023-09-04 17:00:22 +0700

    Implementasikan proses pencarian untuk mencari joko pada array {{budi,LULUS}, {andi,TIDAK

---
+++ src/id/toki/tlx/algo/BinarySearchString2.java
@@ -0,0 +1,46 @@
+package id.toki.tlx.algo;
+
+public class BinarySearchString2 {
+    public static void main(String[] args) {
+        String[][] data = {
+                {"andi", "TIDAK LULUS"},
+                {"budi", "LULUS"},
+                {"cindi", "LULUS"},
+                {"joko", "LULUS"},
+                {"tini", "LULUS"},
+                {"tono", "TIDAK LULUS"}
+        };
+
+        String studentNameToFind = "tini"; // The student name you want to search for
+
+        int index = binarySearch(data, studentNameToFind);
+
+        if (index != -1) {
+            System.out.println("Student '" + studentNameToFind + "' has status: " + data[index][1]);
+        } else {
+            System.out.println("Student '" + studentNameToFind + "' not found in the array.");
+        }
+    }
+
+    // Binary search method
+    public static int binarySearch(String[][] data, String studentName) {
+        int left = 0;
+        int right = data.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int compareResult = studentName.compareTo(data[mid][0]);
+
+            if (compareResult == 0) {
+                return mid; // Found the student, return their index
+            } else if (compareResult < 0) {
+                right = mid - 1; // Student name is in the left half
+            } else {
+                left = mid + 1; // Student name is in the right half
+            }
+        }
+
+        return -1; // Student not found in the array
+    }
+}
--- src/id/toki/tlx/algo/SequentialSearchString.java
+++ src/id/toki/tlx/algo/SequentialSearchString.java
@@ -1,29 +1,30 @@
 package id.toki.tlx.algo;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class SequentialSearchString {
     public static void main(String[] args) {
-        char[] data = {'d', 'c', 'g', 'h', 'b', 'a', 'f', 'i', 'e', 'c'};
-        search(data);
+        String[][] data = {{"budi", "LULUS"},
+                {"andi", "TIDAK LULUS"},
+                {"tini", "LULUS"},
+                {"tono", "TIDAK LULUS"},
+                {"joko", "LULUS"},
+                {"cindi", "LULUS"}};
+        String studentNameToFind = "joko"; // The student name you want to search for
+        String status = sequentialSearch(data, studentNameToFind);
+
+        if (status != null) {
+            System.out.println("Student '" + studentNameToFind + "' has status: " + status);
+        } else {
+            System.out.println("Student '" + studentNameToFind + "' not found in the array.");
+        }
     }
 
-    public static void search(char[] data) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Data Array : " + Arrays.toString(data));
-        System.out.print("\nMasukan angka yang anda cari: ");
-        char key = input.next().charAt(0);
-        for (int index = 0; index < data.length; index++) {
-            System.out.println(data[index] + " != " + key);
-            if (key == data[index]) {
-                System.out.println(key + " == " + data[index] + " yey! ketemu di index ke-" + index);
-                break;
-            }
-
-            if (index == (data.length -1)) {
-                System.out.println("sorry! data tidak ditemukan nih");
+    // Sequential search method
+    public static String sequentialSearch(String[][] data, String studentName) {
+        for (String[] datum : data) {
+            if (datum[0].equals(studentName)) {
+                return datum[1]; // Found the student, return their status
             }
         }
+        return null; // Student not found in the array
     }
 }
