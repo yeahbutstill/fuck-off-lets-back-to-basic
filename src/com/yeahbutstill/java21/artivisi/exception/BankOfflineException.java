b3382d747373f5759b6f326630d7a5ef10d75030
yeahbutstill <mayadani28@gmail.com>
2023-09-29 08:39:55 +0700

    awesome artivisi

---
+++ src/com/yeahbutstill/java21/artivisi/exception/BankOfflineException.java
@@ -0,0 +1,23 @@
+package com.yeahbutstill.java21.artivisi.exception;
+
+public class BankOfflineException extends Exception {
+
+  /**
+   * Constructs a new runtime exception with {@code null} as its detail message. The cause is not
+   * initialized, and may subsequently be initialized by a call to {@link #initCause}.
+   */
+  public BankOfflineException() {
+    super();
+  }
+
+  /**
+   * Constructs a new runtime exception with the specified detail message. The cause is not
+   * initialized, and may subsequently be initialized by a call to {@link #initCause}.
+   *
+   * @param message the detail message. The detail message is saved for later retrieval by the
+   *     {@link #getMessage()} method.
+   */
+  public BankOfflineException(String message) {
+    super(message);
+  }
+}
---
+++ src/com/yeahbutstill/java21/artivisi/exception/TransaksiHariLiburException.java
@@ -0,0 +1,23 @@
+package com.yeahbutstill.java21.artivisi.exception;
+
+public class TransaksiHariLiburException
+    extends Exception { // ini check exception harus di try catch
+  /**
+   * Constructs a new exception with {@code null} as its detail message. The cause is not
+   * initialized, and may subsequently be initialized by a call to {@link #initCause}.
+   */
+  public TransaksiHariLiburException() {
+    super();
+  }
+
+  /**
+   * Constructs a new exception with the specified detail message. The cause is not initialized, and
+   * may subsequently be initialized by a call to {@link #initCause}.
+   *
+   * @param message the detail message. The detail message is saved for later retrieval by the
+   *     {@link #getMessage()} method.
+   */
+  public TransaksiHariLiburException(String message) {
+    super(message);
+  }
+}
