7a30bbe87067cc1d971e35dcaad294bf51d4ca57
yeahbutstill <mayadani28@gmail.com>
2023-10-08 16:55:24 +0700

    awesome record

---
+++ src/com/devoxxx/recordclass/Range.java
@@ -0,0 +1,40 @@
+package com.devoxxx.recordclass;
+
+import java.util.Iterator;
+
+public record Range(int begin, int end) implements Iterable<Integer> {
+  public Range {
+    if (end() < begin()) throw new IllegalStateException("End should be greater than begin");
+  }
+
+  public Range(int end) {
+    this(0, end);
+  }
+
+  @Override
+  public Iterator<Integer> iterator() {
+    return new Iterator<Integer>() {
+      int index = begin();
+
+      @Override
+      public boolean hasNext() {
+        return index < end;
+      }
+
+      @Override
+      public Integer next() {
+        return index++;
+      }
+    };
+  }
+
+  public static void main(String[] args) {
+    var range = new Range(0, 5);
+    System.out.println("range = " + range);
+    System.out.println("begin = " + range.begin());
+
+    for (var value : range) {
+      System.out.println("values = " + value);
+    }
+  }
+}
