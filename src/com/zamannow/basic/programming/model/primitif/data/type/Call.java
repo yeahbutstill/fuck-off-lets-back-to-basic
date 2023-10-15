8f48af648f8b9515c808b7e177c96c0e55bd1e69
yeahbutstill <mayadani28@gmail.com>
2023-10-15 19:25:09 +0700

    awesome

---
+++ src/com/zamannow/basic/programming/model/primitif/data/type/Call.java
@@ -0,0 +1,9 @@
+package com.zamannow.basic.programming.model.primitif.data.type;
+
+import java.io.IOException;
+
+public class Call {
+    public static void main(String[] args) throws IOException {
+        int key = System.in.read();
+    }
+}
