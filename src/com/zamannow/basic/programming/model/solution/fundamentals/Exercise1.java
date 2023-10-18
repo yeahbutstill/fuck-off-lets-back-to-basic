912c6e7adec682453156bd709aca0f06c5e5c872
yeahbutstill <mayadani28@gmail.com>
2023-10-18 11:08:19 +0700

    awesome

---
+++ src/com/zamannow/basic/programming/model/solution/fundamentals/Exercise1.java
@@ -0,0 +1,12 @@
+package com.zamannow.basic.programming.model.solution.fundamentals;
+
+public class Exercise1 {
+  public static void main(String[] args) {
+    int a = (0 + 15) / 2;
+    double b = 2.0e-6 * 100000000.1;
+    boolean c = true && false || true && true;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
---
+++ src/com/zamannow/basic/programming/model/solution/fundamentals/Exercise4.java
@@ -0,0 +1,15 @@
+package com.zamannow.basic.programming.model.solution.fundamentals;
+
+
+public class Exercise4 {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c;
+    // if (a > b) then c = 0; // No such keyword as "then" in Java language
+    // if (a > b) {c = 0;} // Missing parentheses on if conditional
+    // if (a > b) c = 0; // Nothing wrong
+    // if (a > b) c = 0 else b = 0; // Missing semicolon after the "then" clause
+  }
+
+}
