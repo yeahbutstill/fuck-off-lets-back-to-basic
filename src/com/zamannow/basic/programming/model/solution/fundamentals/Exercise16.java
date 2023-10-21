1ae2953d15e49ea9231608292117b19822cda8e8
yeahbutstill <mayadani28@gmail.com>
2023-10-22 06:10:52 +0700

    awesome

--- src/com/zamannow/basic/programming/model/solution/fundamentals/Exercise16.java
+++ src/com/zamannow/basic/programming/model/solution/fundamentals/Exercise16.java
@@ -6,7 +6,7 @@ public class Exercise16 {
   }
 
   public static String exR1(int n) {
-    if (n <= 0) return "";
-    return exR1(n - 3) + n + exR1(n - 2) + n;
+    if (n <= 0) return ", ";
+    else return exR1(n - 3) + n + exR1(n - 2) + n;
   }
 }
---
+++ src/com/zamannow/basic/programming/model/solution/fundamentals/Exercise17.java
@@ -0,0 +1,18 @@
+package com.zamannow.basic.programming.model.solution.fundamentals;
+
+public class Exercise17 {
+  public static void main(String[] args) {
+    System.out.println(exR2(3));
+  }
+
+  public static String exR2(int n) {
+    String s = exR2(n - 3) + n + exR2(n - 2) + n;
+    if (n <= 0) return "";
+    return s;
+  }
+}
+/***
+ * Jawaban : Kasus dasar tidak akan pernah tercapai.
+ * Panggilan ke exR2(3) akan menghasilkan panggilan ke exR2(0), exR2(-3), exR3(-6),
+ * dan seterusnya hingga terjadi StackOverflowError.
+ */
