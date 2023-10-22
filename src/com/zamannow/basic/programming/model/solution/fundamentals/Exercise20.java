0a2e28c83a0a1ce35ee1a2cbd3ebcfa0ec4d324b
yeahbutstill <mayadani28@gmail.com>
2023-10-23 06:07:30 +0700

    awesome

---
+++ src/com/zamannow/basic/programming/model/solution/fundamentals/Exercise20.java
@@ -0,0 +1,18 @@
+package com.zamannow.basic.programming.model.solution.fundamentals;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise20 {
+  public static void main(String[] args) {
+    StdOut.println("ln(5!) = " + inFactorial(5));
+    StdOut.println("Expected: 4.787491742782046");
+  }
+
+  public static double inFactorial(int n) {
+    if (n == 1) {
+      return 0;
+    }
+
+    return Math.log(n) + inFactorial(n - 1);
+  }
+}
