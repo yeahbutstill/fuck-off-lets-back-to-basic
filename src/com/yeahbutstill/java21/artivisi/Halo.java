8ae766d0e1b2a6c2ee57eda9c28d7459530e700c
yeahbutstill <mayadani28@gmail.com>
2023-10-26 05:34:13 +0700

    contoh kode program

---
+++ src/com/yeahbutstill/java21/artivisi/Halo.java
@@ -0,0 +1,10 @@
+package com.yeahbutstill.java21.artivisi;
+
+import java.time.LocalDateTime;
+
+public class Halo {
+  public static void main(String[] args) {
+    System.out.println("Halo");
+    System.out.println("Waktu saat ini " + LocalDateTime.now());
+  }
+}
