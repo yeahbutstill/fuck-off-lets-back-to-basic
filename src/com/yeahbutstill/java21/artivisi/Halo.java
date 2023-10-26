b6889521a237d00b16b2a2d0ce6e948d79b11a4a
yeahbutstill <mayadani28@gmail.com>
2023-10-26 07:13:01 +0700

    awesome

--- src/com/yeahbutstill/java21/artivisi/Halo.java
+++ src/com/yeahbutstill/java21/artivisi/Halo.java
@@ -1,10 +1,17 @@
 package com.yeahbutstill.java21.artivisi;
 
+import com.yeahbutstill.java21.artivisi.toko.Customer;
 import java.time.LocalDateTime;
 
 public class Halo {
   public static void main(String[] args) {
+    Customer ctr1 = new Customer();
+    ctr1.setNama("Maya");
+    ctr1.setEmail("maya@yeahbutstill.com");
+
     System.out.println("Halo");
     System.out.println("Waktu saat ini " + LocalDateTime.now());
+    System.out.println("Nama Customer: " + ctr1.getNama());
+    System.out.println("Email Customer: " + ctr1.getEmail());
   }
 }
