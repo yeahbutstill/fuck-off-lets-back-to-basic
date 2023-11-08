7ca004b5dc6af459cb59069076dee48f69d778c6
yeahbutstill <mayadani28@gmail.com>
2023-11-08 17:22:17 +0700

    awesome StringBuffer

--- src/com/zamannow/stdclass/TestStringBuffer.java
+++ src/com/zamannow/stdclass/TestStringBuffer.java
@@ -1,4 +1,12 @@
 package com.zamannow.stdclass;
 
 public class TestStringBuffer {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Dani");
+        buffer.append(" ");
+        buffer.append("Setiawan");
+        String namaLengkap = buffer.toString();
+        System.out.println(namaLengkap);
+    }
 }
