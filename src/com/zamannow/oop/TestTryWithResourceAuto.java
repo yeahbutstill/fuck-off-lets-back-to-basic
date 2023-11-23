46c996f02b570601fd31434543dc9152d3715664
yeahbutstill <mayadani28@gmail.com>
2023-11-23 11:50:00 +0700

    awesome try with resource

--- src/com/zamannow/oop/TestTryWithResourceAuto.java
+++ src/com/zamannow/oop/TestTryWithResourceAuto.java
@@ -4,6 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * Try with Resource
+ * Di Java 7, terdapat fitur baru yang bernama try with resource
+ * Try with resource ini adalah sebuah mekanisme agar kita lebih mudah menggunakan resource (yang wajib di close) dalam block try
+ * Jika kita ingin menggunakan fitur ini, kita wajib menggunakan interface AutoCloseable
+ */
 public class TestTryWithResourceAuto {
     public static void main(String[] args) {
         // auto close resource
