051bc7e7e28911baad37b6b0961a419e5a072b75
yeahbutstill <mayadani28@gmail.com>
2023-11-23 11:26:02 +0700

    awesome

--- src/com/zamannow/oop/exception/DatabaseError.java
+++ src/com/zamannow/oop/exception/DatabaseError.java
@@ -1,5 +1,12 @@
 package com.zamannow.oop.exception;
 
+/**
+ * Error adalah jenis exception yang tidak direkomendasikan untuk di try-catch
+ * Biasanya error terjadi ketika ada masalah serius, dan sangat tidak direkomendasikan untuk di try-catch.
+ * Artinya, direkomendasikan untuk mematikan aplikasi
+ * Contoh: misal jika diawal aplikasi kita tidak bisa terkoneksi ke database, makan direkomendasikan untuk membuat
+ * Exception yang jenisnya Error dan menghentikan aplikasi
+ */
 public class DatabaseError extends Error {
     public DatabaseError() {
         super();
