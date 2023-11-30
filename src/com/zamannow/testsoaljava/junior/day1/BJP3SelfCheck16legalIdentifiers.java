cc50a12dc1cca586af6d6d5c73dd2688f7c2ec39
yeahbutstill <mayadani28@gmail.com>
2023-11-30 20:15:46 +0700

    legalIdentifiers

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck16legalIdentifiers.java
@@ -0,0 +1,19 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+/**
+ * yang di komen tidak boleh untuk identifiers
+ */
+public class BJP3SelfCheck16legalIdentifiers {
+    public static void main(String[] args) {
+        var B4 = 1;
+//        var 42isThesolution = 2;
+        var println = 3;
+//        var "hello" = 4;
+        var ABC = 5;
+        var AnnualSalary = 6;
+//        var for = 7;
+        var _average = 8;
+//        var first-name = 9;
+        var sum_of_data = 10;
+    }
+}
