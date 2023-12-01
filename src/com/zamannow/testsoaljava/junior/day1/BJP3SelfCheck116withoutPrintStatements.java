19dfd7d480ed3cdfe66ba1bdd5991806182fb6e6
yeahbutstill <mayadani28@gmail.com>
2023-12-01 15:42:11 +0700

    done

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck116withoutPrintStatements.java
@@ -0,0 +1,23 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck116withoutPrintStatements {
+    public static void main(String[] args) {
+        System.out.print("Twas ");
+        System.out.print("brillig and the ");
+        System.out.println(" ");
+        System.out.print("  slithy toves did");
+        System.out.print(" ");
+        System.out.println("gyre and");
+        System.out.println(  "gimble");
+        System.out.println();
+        System.out.println(  "in the wabe." );
+
+        System.out.println("===============================");
+
+        System.out.println("Twas brillig and the ");
+        System.out.println("  slithy toves did gyre and");
+        System.out.println("gimble");
+        System.out.println();
+        System.out.println("in the wabe.");
+    }
+}
