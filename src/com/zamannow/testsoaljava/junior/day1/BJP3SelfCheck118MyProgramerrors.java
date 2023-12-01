56b101897bd67f43a0eec115acb73ae20c807b80
yeahbutstill <mayadani28@gmail.com>
2023-12-01 17:04:25 +0700

    done

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck118MyProgramerrors.java
@@ -0,0 +1,8 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck118MyProgramerrors {
+    public static void main(String[] args) {
+        System.out.println("This is a test of the");
+        System.out.println("emergency broadcast system.");
+    }
+}
---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck119SecretMessageerrors.java
@@ -0,0 +1,8 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck119SecretMessageerrors {
+    public static void main(String[] args) {
+        System.out.println("Speak friends");
+        System.out.println("and enter");
+    }
+}
