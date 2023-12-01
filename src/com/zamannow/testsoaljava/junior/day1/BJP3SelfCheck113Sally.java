d89414f09eceb9f2b6473d59882d1398e3445586
yeahbutstill <mayadani28@gmail.com>
2023-12-01 15:11:20 +0700

    done

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck113Sally.java
@@ -0,0 +1,9 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck113Sally {
+    public static void main(String[] args) {
+        System.out.println("\"Several slashes are sometimes seen,\"");
+        System.out.println("said Sally. \"I've said so.\" See?");
+        System.out.println("\\ / \\\\ // \\\\\\ ///");
+    }
+}
