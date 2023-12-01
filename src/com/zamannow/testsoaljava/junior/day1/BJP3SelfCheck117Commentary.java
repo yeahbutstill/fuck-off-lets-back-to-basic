a4c73e4a0ab1407c544e6e260732e951513481b9
yeahbutstill <mayadani28@gmail.com>
2023-12-01 16:50:02 +0700

    done

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck117Commentary.java
@@ -0,0 +1,15 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck117Commentary {
+    public static void main(String[] args) {
+        System.out.println("some lines of code");
+        System.out.println("have // characters on them");
+        System.out.println("which means ");
+//        System.out.println();
+        System.out.println("lines can also");
+        System.out.println("have /* and */ characters");
+//        System.out.println();
+//        System.out.println();
+        System.out.println("of comment.");
+    }
+}
