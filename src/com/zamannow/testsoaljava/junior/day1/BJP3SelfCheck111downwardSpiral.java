9b159338ac8d25945782665b1c64f6778fdfc8e7
yeahbutstill <mayadani28@gmail.com>
2023-12-01 15:01:41 +0700

    awesome

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck111downwardSpiral.java
@@ -0,0 +1,11 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck111downwardSpiral {
+    public static void main(String[] args) {
+        System.out.println("\ta\tb\tc");
+        System.out.println("\\\\");
+        System.out.println("'");
+        System.out.println("\"\"\"");
+        System.out.println("C:\nin\the downward spiral");
+    }
+}
---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck112DoubleSlash.java
@@ -0,0 +1,12 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck112DoubleSlash {
+    public static void main(String[] args) {
+        System.out.println("Dear \"DoubleSlash\" magazine,");
+        System.out.println();
+        System.out.println("\tYour publication confuses me.  Is it");
+        System.out.println("a \\\\ slash or a //// slash?");
+        System.out.println("\nSincerely,");
+        System.out.println("Susan \"Suzy\" Smith");
+    }
+}
