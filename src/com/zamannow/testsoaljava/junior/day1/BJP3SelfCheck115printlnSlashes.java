8ccac501aa83cdcdbe1b66baef44ab01973c5e83
yeahbutstill <mayadani28@gmail.com>
2023-12-01 15:24:52 +0700

    done

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck115printlnSlashes.java
@@ -0,0 +1,7 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck115printlnSlashes {
+    public static void main(String[] args) {
+        System.out.println("/ \\ // \\\\ /// \\\\\\");
+    }
+}
