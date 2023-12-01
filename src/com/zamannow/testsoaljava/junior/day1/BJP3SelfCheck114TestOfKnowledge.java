4bda0a8ddab3ac15c6fc8ebe5a6935142840a2c4
yeahbutstill <mayadani28@gmail.com>
2023-12-01 15:22:51 +0700

    done

---
+++ src/com/zamannow/testsoaljava/junior/day1/BJP3SelfCheck114TestOfKnowledge.java
@@ -0,0 +1,13 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class BJP3SelfCheck114TestOfKnowledge {
+    public static void main(String[] args) {
+        System.out.println("This is a test of your");
+        System.out.println("knowledge of \"quotes\" used");
+        System.out.println("in \'string literals.\'");
+        System.out.println();
+        System.out.println("You\'re bound to \"get it right\"");
+        System.out.println("if you read the section on");
+        System.out.println("\'\'quotes.\'\'");
+    }
+}
