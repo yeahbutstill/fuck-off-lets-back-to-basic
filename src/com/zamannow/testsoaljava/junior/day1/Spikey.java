cd82ab65d2505ebebb94263dca512fe99db8d3c6
yeahbutstill <mayadani28@gmail.com>
2023-12-02 12:33:14 +0700

    awesome

---
+++ src/com/zamannow/testsoaljava/junior/day1/Spikey.java
@@ -0,0 +1,12 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class Spikey {
+    public static void main(String[] args) {
+        System.out.println("  \\/ ");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+    }
+}
---
+++ src/com/zamannow/testsoaljava/junior/day1/Stewie.java
@@ -0,0 +1,10 @@
+package com.zamannow.testsoaljava.junior.day1;
+
+public class Stewie {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println(" \\/ ");
+    }
+}
