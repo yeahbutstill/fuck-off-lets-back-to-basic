0ff888fe528b703b58a1201e5d41fa8e74ef9d97
yeahbutstill <mayadani28@gmail.com>
2023-12-10 21:39:26 +0700

    awesome

---
+++ src/com/zamannow/testsoaljava/junior/day3/CharMystery.java
@@ -0,0 +1,18 @@
+package com.zamannow.testsoaljava.junior.day3;
+
+public class CharMystery {
+    public static void main(String[] args) {
+        printRange('e', 'g');
+        printRange('n', 's');
+        printRange('z', 'a');
+        printRange('q', 'r');
+        printRange('a', 'z');
+    }
+
+    private static void printRange(char startLetter, char endLatter) {
+       for (char letter = startLetter; letter <= endLatter; letter++) {
+           System.out.print(letter);
+       }
+        System.out.println();
+    }
+}
